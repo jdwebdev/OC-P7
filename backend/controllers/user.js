3e97e6880124534e6b439ca7443f50e1732b377f
jd <jdwebdev974@gmail.com>
2020-08-28 22:25:49 +0200

    add login route and jwt token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const models = require('../models');
 const { Op } = require('sequelize');
+require("dotenv").config();
 
 exports.signup = (req, res, next) => {
     
@@ -13,7 +14,6 @@ exports.signup = (req, res, next) => {
         return res.status(400).json({ 'error': 'Paramètres manquants' });
     }
 
-    
     
     models.User.findOne({
         attributes: ['username', 'email'],
@@ -51,8 +51,41 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-    // FindOne.. bcrypt.compare ...
+
     const email  = req.body.email;
     const password = req.body.password;
     
+    if (email == null || password == null) {
+        return res.status(400).json({ 'error': 'Paramètres manquants' });
+    }
+
+
+    models.User.findOne({
+        where: { email: email }
+    })
+    .then((user) => {
+        if (user) {
+            bcrypt.compare(password, user.password)
+                .then( valid => {
+                    if (!valid) {
+                        return res.status(401).json({ 'error': 'Mot de passe incorrect '});
+                    } 
+                    res.status(200).json({
+                        userId: user.id,
+                        token: jwt.sign({ 
+                                userId: user.id,
+                                isAdmin: user.isAdmin 
+                            },
+                            process.env.SECRET_TOKEN,
+                            { expiresIn: "1h" }
+                        )
+                    })
+                })
+        } else {
+            return res.status(404).json({ 'error': `Cet utilisateur n'existe pas`});
+        }
+    })
+    .catch(() => {
+        return res.status(500).json({ 'error': `Impossible de vérifier l'utilisateur`});
+    })
 }
